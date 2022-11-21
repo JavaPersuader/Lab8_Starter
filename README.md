63aa1488ed3448a01b773b45182fb029d77e7f28
JavaPersuader <liuchuning831@gmail.com>
2022-11-20 17:01:51 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Lab 8 - Starter
+## 1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+1. Within a Github action that runs whenever code is pushed 
+   * No.1 is the answer. Because this test ensures that the code can be tested automatically every time it is pushed to the github repository, preventing code crashes. It also allows team members to check the cause of code crashes in time for them to discuss and make changes.
+
+## 2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+* This doesn't really require an E2E test, but rather a unit test. Since this is really just testing whether a function returns the correct value, a unit test is more appropriate than an E2E test.
+
+
+
+## 3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+* This is because the message function function mainly allows the user to write and send. These functions require different formations to connect and interact. So we can't use unit tests.
+
+
+
+## 4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+* Yes, unit testing is needed for this function. Because it is a small function. He only needs to test if the user input is more than 80 characters. So we only need a unit test to detect whether this function is correct or not.
+
+
+## Lab8.js test screen shoot
+Here is the lab8 image:
+
+![lab8](./ScreenShots/lab8.test.js.png)
+## sum.js test screen shoot
+Here is the sum.test image:
+
+![sum](./ScreenShots/sum.test.png)
+
+## unit.js test screen shoot
+Here is the unit.test image:
+
+![sum](./ScreenShots/unit.test.png)
+
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,86 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
+
 // TODO - Part 2
+
+
+test('Check if (626)887-1234 is a phone Number', () => {
+    expect(functions.isPhoneNumber("(626)887-1234")).toBe(true);
+});
+
+test('Check if (858)238-7313 is a phone Number', () => {
+    expect(functions.isPhoneNumber("(858)238-7313")).toBe(true);
+});
+
+test('Check if 1823749032 is not a phone number', () => {
+    expect(functions.isPhoneNumber("1823749032")).toBe(false);
+});
+
+test('Check if 193%^@1839 is not a phone number', () => {
+    expect(functions.isPhoneNumber("193%^@1839")).toBe(false);
+});
+
+test('Check if helloWorld@gmail.com is an Email', () => {
+    expect(functions.isEmail("helloWorld@gmail.com")).toBe(true);
+});
+
+test('Check if helloWorld@ucsd.edu is an Email', () => {
+    expect(functions.isEmail("helloWorld@ucsd.edu")).toBe(true);
+});
+
+test('Check if helloWorld$gmail.com is not an Email', () => {
+    expect(functions.isEmail("helloWorld$gmail.com")).toBe(false);
+});
+
+test('Check if helloWorld$@@ucsd.edu is not an Email', () => {
+    expect(functions.isEmail("helloWorld$@@ucsd.edu")).toBe(false);
+});
+
+test('Check if aisdFjeak is a strong password', () => {
+    expect(functions.isStrongPassword("aisdFjeak")).toBe(true);
+});
+
+test('Check if jaksjfiea is a strong password', () => {
+    expect(functions.isStrongPassword("jaksjfiea")).toBe(true);
+});
+
+test('Check if 12489129#￥#……&@# is not a strong password', () => {
+    expect(functions.isStrongPassword("12489129#￥#……&@#")).toBe(false);
+});
+
+test('Check if 2376#……@ is not a strong password', () => {
+    expect(functions.isStrongPassword("2376#……@")).toBe(false);
+});
+
+test('Check if 11/17/2022 is a date', () => {
+    expect(functions.isDate("11/17/2022")).toBe(true);
+});
+
+test('Check if 08/11/2018 is a date', () => {
+    expect(functions.isDate("08/11/2018")).toBe(true);
+});
+
+test('Check if 99/88/A999 is not a date', () => {
+    expect(functions.isDate("99/88/A999")).toBe(false);
+});
+
+test('Check if 23/A1/1234 is not a date', () => {
+    expect(functions.isDate("23/A1/1234")).toBe(false);
+});
+
+test('Check if #FFFFFF is a hex color', () => {
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+});
+
+test('Check if #a23FE2 is a hex color', () => {
+    expect(functions.isHexColor("#a23FE2")).toBe(true);
+});
+
+test('Check if #FFFFFFF isnt a hex color', () => {
+    expect(functions.isHexColor("#FFFFFFF")).toBe(false);
+});
+
+test('Check if F isnt a hex color', () => {
+    expect(functions.isHexColor("F")).toBe(false);
+});
